54736b1225b48e80e2f090625a47aaa720bece21
Agustin Laner <agustinnlanerr@gmail.com>
2025-10-23 14:28:46 -0300

    Profesor.java modelo

---
+++ src/main/java/com/is1/proyecto/models/Profesor.java
@@ -0,0 +1,44 @@
+import org.javalite.activejdbc.Model;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+
+@Table("profesores") // Asocia el modelo con la tabla 'profesores' (ya creada en schema.sql)
+public class Profesor extends Model {
+
+    // Igual que en User.java, usamos getters/setters simples
+    // ActiveJDBC ya mapea columnas -> atributos del modelo
+
+    public String getNombre() {
+        return getString("nombre");
+    }
+
+    public void setNombre(String nombre) {
+        set("nombre", nombre);
+    }
+
+    public String getApellido() {
+        return getString("apellido");
+    }
+
+    public void setApellido(String apellido) {
+        set("apellido", apellido);
+    }
+
+    public String getCorreo() {
+        return getString("correo");
+    }
+
+    public void setCorreo(String correo) {
+        set("correo", correo);
+    }
+
+    public String getDni() {
+        return getString("dni");
+    }
+
+    public void setDni(String dni) {
+        set("dni", dni);
+    }
+}
